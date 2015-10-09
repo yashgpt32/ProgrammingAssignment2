fc1df8b4dc798ca4973dbb1de4b19aeb225f6f99
yashgpt32 <yashgpt32@gmail.com>
2015-10-09 23:05:18 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,32 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  i<-NULL
+  setmatrix<-function(y)
+  {
+    x<<-y
+  }
+  getmatrix<-function() x
+  setinverse<-function(inverse) i<<-inverse
+  getinverse<-function() i
+  list(getmatrix=getmatrix,setinverse=setinverse,getinverse=getinverse)
+  
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+i<-x$getinverse()
+  if(!is.null(i))
+  {
+    message("cacheing inverse")
+    return(i)
+  }
+  data<-x$getmatrix()
+  inverse=solve(data)
+  x$setinverse(inverse) 
+  inverse
         ## Return a matrix that is the inverse of 'x'
 }
